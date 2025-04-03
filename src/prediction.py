4cb11bc1dcac7aeb7171d33f3ccd346f7fb646c9
Liliane Kayitesi <125905987+lilika67@users.noreply.github.com>
2025-04-04 00:15:14 +0200

    created EzanAi backend apis

--- src/prediction.py
+++ src/prediction.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Crop Yield Prediction API")
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
